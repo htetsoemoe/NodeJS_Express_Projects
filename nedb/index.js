c4293fbfdafb3538e5c1804f3c0f4c2f35732213
Soe Moe Htet <92364327+htetsoemoe@users.noreply.github.com>
2022-10-02 16:26:17 +0630

    first commit
    Node.js, Express Framework and NeDB

---
+++ nedb/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dataStore = require('nedb');
+
+const app = express();
+app.listen(3000, () => console.log('Listening at Port 3000.'));
+app.use(express.static('public'));
+app.use(express.json({ limit: "1mb"}));
+
+// creating in-memory database
+const database = new dataStore('nedb.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    console.log("I got a request!");
+    const data = request.body;
+
+    // create current time-stamp
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+
+    // insert data to db
+    database.insert(data);
+    console.log(data);
+    // response with json to client
+    response.json(data);
+})
